9feda1191db88bd2ab85356c19c5500988c55303
nowod <ldowon1223@naver.com>
2024-03-13 17:23:46 +0900

    업로드 기능 구현

---
+++ src/main/java/file/FileDTO.java
@@ -0,0 +1,19 @@
+package file;
+
+public class FileDTO {
+	
+	String fielName;
+	String fileRealName;
+	public String getFielName() {
+		return fielName;
+	}
+	public void setFielName(String fielName) {
+		this.fielName = fielName;
+	}
+	public String getFileRealName() {
+		return fileRealName;
+	}
+	public void setFileRealName(String fileRealName) {
+		this.fileRealName = fileRealName;
+	}
+}
